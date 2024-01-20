0dc88d1ed281b0363e6e011bf598a3337423dc7a
luandev <luannguyenn.2k2@gmail.com>
2024-01-20 16:17:30 +0700

    dockerize:v1

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-const dotenv = require("dotenv");
-const express = require("express");
-const { createServer } = require("http");
-const { Server } = require("socket.io");
-const roomHandler = require("./roomHandler");
+const dotenv = require("dotenv")
+const express = require("express")
+const { createServer } = require("http")
+const { Server } = require("socket.io")
+const roomHandler = require("./roomHandler")
 
-dotenv.config();
-const app = express();
+dotenv.config()
+const app = express()
 
-const httpServer = createServer(app);
+const httpServer = createServer(app)
 const io = new Server(httpServer, {
   cors: { origin: "*" },
-});
+})
 
-const rooms = [];
+const rooms = []
 
 io.on("connection", (socket) => {
-  console.log("connected", socket.id);
-  roomHandler(io, socket, rooms);
+  console.log("connected", socket.id)
+  roomHandler(io, socket, rooms)
 
   socket.on("disconnect", () => {
-    console.log("disconnected", socket.id);
-  });
-});
+    console.log("disconnected", socket.id)
+  })
+})
 
-const port = process.env.PORT || 8080;
-httpServer.listen(port, () => console.log(`Listening on port ${port}`));
+const port = process.env.PORT || 8080
+httpServer.listen(port, "0.0.0.0", () =>
+  console.log(`Listening on port ${port}`)
+)
